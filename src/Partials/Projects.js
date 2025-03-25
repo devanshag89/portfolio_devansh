2b2ab98538651d84d85307b23ec0f5728def2979
Devansh Agrawal <122165428+devanshcs22@users.noreply.github.com>
2025-03-25 13:49:15 +0530

    commit

--- src/Partials/Projects.js
+++ src/Partials/Projects.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from 'react';
 import ProjectCard from "./ProjectCard";
 
 const Projects = ({ projects }) => {
-  // Slice the projects array to show only the first three projects
-  const displayedProjects = projects.slice(0, 3);
-  
+  const [showAllProjects, setShowAllProjects] = useState(false);
+
+  // Determine which projects to display based on showAllProjects state
+  const displayedProjects = showAllProjects ? projects : projects.slice(0, 3);
+
+  const toggleProjectsView = () => {
+    setShowAllProjects(!showAllProjects);
+  };
+
   return (
     <div className="p-10">
       <h1 className="mt-8 text-2xl md:text-5xl text-center gradient-heading pb-6 font-bold">My projects</h1>
       <div className="flex flex-col lg:flex-row align-center sm:items-center md:items-center justify-center max-w-8xl mx-auto mt-8 gap-10 pb-10">
-        {displayedProjects.map((project, index) => (
+        {displayedProjects.slice(0, 3).map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
       </div>
+
+      {showAllProjects && displayedProjects.length > 3 && (
+        <div className="flex flex-col lg:flex-row align-center sm:items-center md:items-center justify-center max-w-8xl mx-auto mt-8 gap-10 pb-10">
+          {displayedProjects.slice(3).map((project, index) => (
+            <ProjectCard key={index + 3} project={project} />
+          ))}
+        </div>
+      )}
+
+      {projects.length > 3 && (
+        <div className="flex justify-center">
+          <button 
+            onClick={toggleProjectsView}
+            className="transition-all duration-700 hover:scale-110 bg-gradient-to-t from-orange-600 to-yellow-600 hover:bg-emerald-30 text-lg font-semibold py-2 px-4 rounded-md text-white"
+          >
+            {showAllProjects ? 'View Less' : 'View More'}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/Partials/Skills.js
+++ src/Partials/Skills.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from 'react';
 import SkillCard from "./SkillCard";
 
 function Skills({ skills }) {
+  const [showAllSkills, setShowAllSkills] = useState(false);
+
+  // Determine which skills to display based on showAllSkills state
+  const displayedSkills = showAllSkills ? skills : skills.slice(0, 5);
+
+  const toggleSkillsView = () => {
+    setShowAllSkills(!showAllSkills);
+  };
+
   return (
     <div className="mt-20">
-      <h1 className="mt-8 text-2xl md:text-5xl text-center gradient-heading pb-6 font-bold">My Skills</h1>
-      <div className="flex flex-wrap justify-center max-w-5xl mx-auto mt-8">
-        {skills.slice(0, 5).map((skill, index) => (
-          <SkillCard 
+      <h1 className="mt-8 text-2xl md:text-5xl text-center gradient-heading pb-6 font-bold">
+        My Skills
+      </h1>
+      
+      <div className="grid grid-cols-2 md:grid-cols-5 gap-4 max-w-5xl mx-auto mt-8">
+        {displayedSkills.map((skill, index) => (
+          <SkillCard
             key={index}
-            skillName={skill.skillName} 
-            skillIcon={skill.skillIcon} 
+            skillName={skill.skillName}
+            skillIcon={skill.skillIcon}
           />
         ))}
       </div>
+
+      {skills.length > 5 && (
+        <div className="flex justify-center mt-8">
+          <button 
+            onClick={toggleSkillsView}
+            className="transition-all duration-700 hover:scale-110 bg-gradient-to-t from-orange-600 to-yellow-600 hover:bg-emerald-30 text-lg font-semibold py-2 px-4 rounded-md text-white"
+          >
+            {showAllSkills ? 'View Less' : 'View More'}
+          </button>
+        </div>
+      )}
     </div>
   );
 }
